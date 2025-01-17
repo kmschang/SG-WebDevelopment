265fff0ba3cb98cd09e9dcd7141d6fcecaa04589
Kyle Schang <kyle@schang.net>
2025-01-17 09:49:51 -0500

    updating .html to .php

--- privacy.php
+++ privacy.php
@@ -2,22 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Home | Sonnaz Group</title>
+    <title>Privacy | Sonnaz Group</title>
     <link rel="icon" type="image/x-icon" href="Assets/favicon.png">
 </head>
 <body>
 <?php
-/*
-    One Header Option:
-    - Cardinal_Header.html (Original | Like current website)
- */
 include 'includes/SonnazGroup_Header.html';
-include 'terms.html';
-/*
-    Two Footer Options:
-    - Cardinal_Footer.html (Original | Like current website)
-    - Cardinal_Footer2.html (More minimal | More modern)
-*/
+include 'includes/SonnazGroup_Privacy.html';
 include 'includes/SonnazGroup_Footer.html';
 ?>
 <script src="js/bootstrap.min.js"></script>
