2cdd2f295f4da23ed63f3e4c517c1ebae6274f63
Kyle Schang <kyle@schang.net>
2025-01-17 09:36:46 -0500

    trying to get php working

---
+++ SonnazGroup_Home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home | Sonnaz Group</title>
+    <link rel="icon" type="image/x-icon" href="Assets/favicon.png">
+</head>
+<body>
+<?php
+/*
+    One Header Option:
+    - Cardinal_Header.html (Original | Like current website)
+ */
+include 'includes/SonnazGroup_Header.html';
+include 'terms.html';
+/*
+    Two Footer Options:
+    - Cardinal_Footer.html (Original | Like current website)
+    - Cardinal_Footer2.html (More minimal | More modern)
+*/
+include 'includes/SonnazGroup_Footer.html';
+?>
+</body>
+</html>
