6cf7c3bcfb159147bfccf4bdff24b2561d16d54e
Kyle Schang <kyle@schang.net>
2025-01-19 12:53:22 -0500

    transfering everything to php

---
+++ dayTracker.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Day Tracker | Sonnaz Group</title>
+    <link rel="icon" type="image/x-icon" href="Assets/favicon.png">
+</head>
+<body>
+<?php
+include 'includes/SonnazGroup_Header.html';
+include 'includes/SonnazGroup_Footer.html';
+?>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ privacy.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Privacy | Sonnaz Group</title>
+    <link rel="icon" type="image/x-icon" href="Assets/favicon.png">
+</head>
+<body>
+<?php
+include 'includes/SonnazGroup_Header.html';
+include 'includes/SonnazGroup_Privacy.html';
+include 'includes/SonnazGroup_Footer.html';
+?>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
