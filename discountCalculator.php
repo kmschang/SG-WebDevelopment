17cd2666128eb6e8122afb9567eec3b24a00a6e0
Kyle Schang <kyle@schang.net>
2025-01-20 17:07:48 -0500

    fixed links and JS

--- discountCalculator.php
+++ discountCalculator.php
@@ -8,6 +8,7 @@
 <body style="background: #1B1C1E">
 <?php
 include 'includes/SonnazGroup_Header.html';
+include 'includes/SonnazGroup_ComingSoon.html';
 include 'includes/SonnazGroup_Footer.html';
 ?>
 <script src="js/bootstrap.min.js"></script>
