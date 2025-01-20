bed0de57c8e7674cdc81488a210529ec85452528
Kyle Schang <kyle@schang.net>
2025-01-20 12:00:46 -0500

    added all remaining pages

---
+++ FAQ.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FAQ | Sonnaz Group</title>
+    <link rel="icon" type="image/x-icon" href="Assets/favicon.png">
+</head>
+<body style="background: #1B1C1E">
+<?php
+include 'includes/SonnazGroup_Header.html';
+include 'includes/SonnazGroup_ComingSoon.html';
+include 'includes/SonnazGroup_Footer.html';
+?>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
