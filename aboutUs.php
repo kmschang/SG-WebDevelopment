87003cad2d7efcbb66a01c1c412d37f5db6fea06
Kyle Schang <kyle@schang.net>
2025-02-19 11:45:22 -0500

    about us v2 is onge

--- aboutUs.php
+++ aboutUs.php
@@ -8,7 +8,7 @@
 <body style="background: #1B1C1E">
 <?php
 include 'includes/SonnazGroup_Header.html';
-include 'includes/SonnazGroup_AboutUs.html';
+include 'includes/SonnazGroup_AboutUs2.html';
 include 'includes/SonnazGroup_Footer.html';
 ?>
 <script src="js/bootstrap.min.js"></script>
